3ec674f12f712bc97d5b8beb1e4509f2c27d1cad
ACER <mhdiqbaltan@gmail.com>
2020-04-18 17:52:52 +0700

    styling-1

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(function() {
+	$(document).scroll(function() {
+		var $nav = $('#navbarUtama');
+		$nav.toggleClass('scrolled', $(this).scrollTop() > 380);
+	});
+});
+
+var onePageClick = function() {
+	$(document).on('click', '#navku a[href^="#"]', function(event) {
+		event.preventDefault();
+
+		var href = $.attr(this, 'href');
+
+		$('html, body').animate(
+			{
+				scrollTop: $($.attr(this, 'href')).offset().top - 70
+			},
+			500,
+			function() {
+				// window.location.hash = href;
+			}
+		);
+	});
+};
+
+onePageClick();
